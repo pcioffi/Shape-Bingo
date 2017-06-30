1b9a463f85bffa905cabd5d8e8495e9361a2f097
pcioffi <pellegrinocioffi93@gmail.com>
2017-06-30 00:06:34 -0400

    First commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Button tileButton;
+	public List <string> shapeName;
+	public List <Sprite> shapeSprite;
+	public List <Sprite> highlightSprite;
+
+	// Use this for initialization
+	void Start () {
+		//Initialize Bingo Tiles with randomly assigned shapes
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeShape(){
+		tileButton.transform.Find("Shape").GetComponent<Image>().sprite = shapeSprite[0];
+	}
+}
